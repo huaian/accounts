20762964d7b1062a539ef492661be5c95d166c83
cloud <chenxiangyun01@chinatopcredit.com>
2016-10-12 17:49:58 +0800

    []

--- public/accounts/app/appRestStore/system_notification.js
+++ public/accounts/app/appRestStore/system_notification.js
@@ -38,21 +38,17 @@ function (
 
   _stores._name = 'pc/store/restStores';
 
-  //下载通话音频文件
-  //use template http://10.3.1.241/loaning-web/call-center/record/file?fileUrl=http://10.21.2.76/voices/record/20160712/4000006-20160712163805-13717089090-075522733134-record-ztxcs76m-1468312685.61210.mp3&uniqueId=ztxcs76m-1468312685.61210
+  //系统通知
+  _stores.announcement = new CRest({
+    target: urlPrefix + '/common/announcement/',
+    authRequired:true
+  });
 
-
-    //系统通知
-    _stores.announcement = new CRest({
-      target: urlPrefix + '/common/announcement/',
-      authRequired:true
-    });
-
-    //系统通知
-    _stores.announcements = new CRest({
-      target: urlPrefix + '/common/announcements/',
-      authRequired:true
-    });
+  //系统通知
+  _stores.announcements = new CRest({
+    target: urlPrefix + '/common/announcements/',
+    authRequired:true
+  });
 
   return _stores;
 });
