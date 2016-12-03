a1c8d91d8f62c342549f5565fe88a3a6cc3bd645
cloud <chenxiangyun01@chinatopcredit.com>
2016-12-03 17:14:02 +0800

    [格式化代码]

--- routes/accounts/statistics.js
+++ routes/accounts/statistics.js
@@ -10,16 +10,16 @@ var _ = require('underscore');
 var sha1 = require('sha1');//加密 生成jsapi调用签名
 //app js
 var Db = require('mongodb').Db,
-MongoClient = require('mongodb').MongoClient,
-Server = require('mongodb').Server,
-ReplSetServers = require('mongodb').ReplSetServers,
-ObjectID = require('mongodb').ObjectID,
-Binary = require('mongodb').Binary,
-GridStore = require('mongodb').GridStore,
-Grid = require('mongodb').Grid,
-Code = require('mongodb').Code,
-//BSON = require('mongodb').pure().BSON,
-assert = require('assert');
+  MongoClient = require('mongodb').MongoClient,
+  Server = require('mongodb').Server,
+  ReplSetServers = require('mongodb').ReplSetServers,
+  ObjectID = require('mongodb').ObjectID,
+  Binary = require('mongodb').Binary,
+  GridStore = require('mongodb').GridStore,
+  Grid = require('mongodb').Grid,
+  Code = require('mongodb').Code,
+  //BSON = require('mongodb').pure().BSON,
+  assert = require('assert');
 var crypto = require('crypto');
 var ObjectID = require('mongodb').ObjectID;
 //dict data
@@ -35,10 +35,10 @@ console.log(arg.aa);//返回001
 console.log(arg.bb);//返回002
 */
 var current = new Date();
-var currentMonth = new Date(current.getFullYear(),current.getMonth());//当前月开始时间
-var nextMonth = new Date(current.getFullYear(),current.getMonth() + 1);//下个月的时间开始
+var currentMonth = new Date(current.getFullYear(), current.getMonth());//当前月开始时间
+var nextMonth = new Date(current.getFullYear(), current.getMonth() + 1);//下个月的时间开始
 //ADD
-router.get('/',function(req, res, next){
+router.get('/', function (req, res, next) {
   //check auth
   if (!req.user || !req.user.userName) {
     next(new Error("Permission denied."));
@@ -53,125 +53,125 @@ router.get('/',function(req, res, next){
   */
   var startDate = arg.startDate;
   var endDate = arg.endDate;
-  console.log(startDate,endDate,arg);
+  console.log(startDate, endDate, arg);
   var statistics = {};
-  mongoConnect.then(function(db) {
+  mongoConnect.then(function (db) {//request
     var collection = db.collection('incomes');//incomes
     console.log('collection');
     var incomesCursor = collection.aggregate(
       [
         {
-          $project:{
-            date:1,
-            amount:1,
-            userName:1,
-            type:1
+          $project: {
+            date: 1,
+            amount: 1,
+            userName: 1,
+            type: 1
           }
         },
         {
-          $match:{
-          userName:req.user.userName,
-          date:{
-            '$gte':_.isString(startDate)?new Date(startDate):currentMonth,
-            '$lte':_.isString(endDate)?new Date(endDate):nextMonth
-          }
-        }
-      },
-      {
-        $group:{
-          _id:"$type",
-          y:{$sum:"$amount"},
-        }
-      },
-      {
-        $project:{
-          type:"$_id",
-          y: "$y",
-          text: "$_id",
-          tooltip: "$y"
-        }
-      }
-    ]
-  );
-incomesCursor.toArray().then(function(items){
-    console.log(items);
-    statistics.incomes = items;
-    console.log(statistics.incomes);
-    var collection = db.collection('expenses');//expenses
-    /**
-    {
-      y: 1.48,
-      text: "Russian",
-      tooltip: "148 million"
-    }
-    */
-    var expensesCursor = collection.aggregate(
-      [
-        {
-          $project:{
-            date:1,
-            amount:1,
-            userName:1,
-            type:1
-          }
-        },
-        {
-          $match:{
-            userName:req.user.userName,
-            date:{
-              '$gte':_.isString(startDate)?new Date(startDate):currentMonth,
-              '$lte':_.isString(endDate)?new Date(endDate):nextMonth
+          $match: {
+            userName: req.user.userName,
+            date: {
+              '$gte': _.isString(startDate) ? new Date(startDate) : currentMonth,
+              '$lte': _.isString(endDate) ? new Date(endDate) : nextMonth
             }
           }
         },
         {
-          $group:
-          {
-            _id:"$type",
-            y:{
-              $sum:"$amount"
-            }
+          $group: {
+            _id: "$type",
+            y: { $sum: "$amount" },
           }
         },
         {
-          $project:{
-            type:"$_id",
+          $project: {
+            type: "$_id",
             y: "$y",
             text: "$_id",
             tooltip: "$y"
           }
-        },
-    ]
-  );
-  return expensesCursor.toArray();
-}).then(
-  function(items) {
-    statistics.expenses = items;
-    ///*
-    dictUtil.getDictData('income_types').then(function(dictItems){
-      _.each(statistics.incomes,function(item){
-        item.tooltip = item.tooltip.toFixed(2);
-        item.text = (_.findWhere(dictItems,{id:item.type}) || {}).name;//set desc attribute
-      });
-      dictUtil.getDictData('expense_types').then(function(dictItems){
-        _.each(statistics.expenses,function(item){
-          item.tooltip = item.tooltip.toFixed(2);
-          item.text = (_.findWhere(dictItems,{id:item.type}) || {}).name;//set desc attribute
-        });
-        res.json(
+        }
+      ]
+    );
+    incomesCursor.toArray().then(function (items) {
+      console.log(items);
+      statistics.incomes = items;
+      console.log(statistics.incomes);
+      var collection = db.collection('expenses');//expenses
+      /**
+      {
+        y: 1.48,
+        text: "Russian",
+        tooltip: "148 million"
+      }
+      */
+      var expensesCursor = collection.aggregate(//聚合数据
+        [
           {
-            "responseStatus":{
-              "code":"000000",//状态码，"000000"成功,其他为失败 4000004
-              "msg":"查询成功！",//描述
-            },
-            body:statistics
-          }
-        );
+            $project: {
+              date: 1,
+              amount: 1,
+              userName: 1,
+              type: 1
+            }
+          },
+          {
+            $match: {
+              userName: req.user.userName,
+              date: {
+                '$gte': _.isString(startDate) ? new Date(startDate) : currentMonth,//from date
+                '$lte': _.isString(endDate) ? new Date(endDate) : nextMonth//end date
+              }
+            }
+          },
+          {
+            $group:
+            {
+              _id: "$type",
+              y: {
+                $sum: "$amount"
+              }
+            }
+          },
+          {
+            $project: {
+              type: "$_id",
+              y: "$y",
+              text: "$_id",
+              tooltip: "$y"
+            }
+          },
+        ]
+      );
+      return expensesCursor.toArray();//get the 
+    }).then(
+      function (items) {
+        statistics.expenses = items;
+        ///*
+        dictUtil.getDictData('income_types').then(function (dictItems) {//get income dict data list
+          _.each(statistics.incomes, function (item) {
+            item.tooltip = item.tooltip.toFixed(2);
+            item.text = (_.findWhere(dictItems, { id: item.type }) || {}).name;//set desc attribute before send to front-end
+          });
+          dictUtil.getDictData('expense_types').then(function (dictItems) {
+            _.each(statistics.expenses, function (item) {
+              item.tooltip = item.tooltip.toFixed(2);
+              item.text = (_.findWhere(dictItems, { id: item.type }) || {}).name;//set desc attribute before sent to front-end
+            });
+            res.json(//send back success search response
+              {
+                "responseStatus": {
+                  "code": "000000",//状态码，"000000"成功,其他为失败 4000004
+                  "msg": "查询成功！",//描述
+                },
+                body: statistics
+              }
+            );
+          });
+        });
+        //*/
       });
-    });
-    //*/
   });
-});
 });
 
 module.exports = router;
