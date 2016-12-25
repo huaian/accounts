74474f1d36afc1cb2de9963073572612dae74547
cloud <chenxiangyun01@chinatopcredit.com>
2016-12-25 19:47:36 +0800

    ［修复问题］修复编辑支出问题

--- public/accounts/app/h5/view/expenses/view.js
+++ public/accounts/app/h5/view/expenses/view.js
@@ -83,7 +83,7 @@ function (
       });
       if(_.isString(expenseId)){
         expensesRestStores.expenses.get(expenseId).then(function(resp){
-          self.viewData.formData = resp.body.content;
+          self.viewData.formData = resp.body;//获取数据
           self.viewData.formData.date = locale.format(new Date(self.viewData.formData.date), {
             datePattern: "yyyy-MM-dd",
             selector: 'date'
